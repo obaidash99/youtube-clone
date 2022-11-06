119700f151710635fc2e16d428d264cff279c909
obaida <obaidashurbaji99@gmail.com>
2022-11-06 21:22:13 +0200

    channel work

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Box } from '@mui/material';
+
+import Videos from './Videos';
+import VideoCard from './VideoCard';
+import { fetchFromAPI } from '../utilities/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+	const { id } = useParams();
+	const [chennelDetail, setChennelDetail] = useState(null);
+	const [videos, setVideos] = useState([]);
 
-export default ChannelDetail
+	console.log(id, videos);
+
+	useEffect(() => {
+		fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+			setChennelDetail(data?.items[0])
+		);
+
+		fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+			setVideos(data?.items)
+		);
+	}, [id]);
+
+	return <div>{id}</div>;
+};
+
+export default ChannelDetail;
