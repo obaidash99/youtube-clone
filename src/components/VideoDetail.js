8cc675fc607196f6f2b6c512b222361d7fb35780
obaida <obaidashurbaji99@gmail.com>
2022-11-09 12:23:32 +0200

    limit of api for this day exceeded

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,11 +9,16 @@ import { fetchFromAPI } from '../utilities/fetchFromAPI';
 
 const VideoDetail = () => {
 	const [videoDetail, setVideoDetail] = useState(null);
+	const [videos, setVideos] = useState(null);
 	const { id } = useParams();
 
 	useEffect(() => {
 		fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
 			setVideoDetail(data.items[0])
+		);
+
+		fetchFromAPI(`search>part=snippet&relatedToVideoId=${id}&type=video`).then((data) =>
+			setVideos(data.items)
 		);
 	}, [id]);
 
@@ -35,7 +40,7 @@ const VideoDetail = () => {
 							controls
 						/>
 						<Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-							{videoDetail.snippet.title}
+							{title}
 						</Typography>
 						<Stack
 							direction="row"
@@ -60,6 +65,9 @@ const VideoDetail = () => {
 							</Stack>
 						</Stack>
 					</Box>
+				</Box>
+				<Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center">
+					<Videos videos={videos} direction="column" />
 				</Box>
 			</Stack>
 		</Box>
